bed1226c52889121382b6c5b08ebfab4d7387c40
Peter Brock <peter.brock@atos.net>
2016-09-05 11:12:41 +0100

    Initial project setup

---
+++ src/main/java/net/atos/epi/Main.java
@@ -0,0 +1,17 @@
+package net.atos.epi;
+
+/**
+ * Created by peterbrock on 05/09/16.
+ */
+public class Main {
+
+    /**
+     * Entry point of the application
+     * @param args expects a list of items in the basket (e.g. Apple, Orange, Apple, Apple)
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Main executed");
+    }
+
+}
